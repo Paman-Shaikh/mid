60840e89d5193f6743d95f04224ca76f79b08dad
Paman-Shaikh <pamanshaikh.bscsf22@iba-suk.edu.pk>
2025-03-24 19:35:32 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import New from "./assets/print"
+function App() {
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <button> Click me </button>
+      <New />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/print.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+ 
+function New(){
+    return(
+        <div>
+            <h1>Hey this is component</h1>
+        </div>
+    )
+}
+export default New;
